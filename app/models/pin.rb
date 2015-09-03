e69a2438b1676f606c02b42e8da634f8b58e2a30
Robin Nilsson <robin@robins.nu>
2015-09-03 21:49:53 +0200

    Added displaying of users names

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,7 @@ class Pin < ActiveRecord::Base
 
 	has_attached_file :image, :styles => { xlarge: "1000x1000", large: "600x600>", medium: "300x300>", thumb: "100x100>" }
 	validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
+
+	validates :image, presence: true
+  validates :description, presence: true
 end
