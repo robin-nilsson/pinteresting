5668c3026603e3a70c0ba2572790c6374672ec06
Robin Nilsson <robin@robins.nu>
2015-09-04 18:56:35 +0200

    Added Visual Code settings to gitignore. Fixed bug: pins for deleted users were not deleted.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :pins
+  has_many :pins, dependent: :destroy
 
   validates :name, length: { minimum: 2 }, presence: true
 end
